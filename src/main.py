aac62f2c4af26d2118484efa2d1c8e04a29077d8
kaushik327 <kaushik.r.varadharajan@gmail.com>
2024-10-22 13:15:12 -0500

    Simplify wiring with pointers

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ from dataclasses import dataclass
 import numpy as np
 from itertools import product
 
+from typing import Optional
+
 # algorithm from https://ecelabs.njit.edu/ece459/lab3.php
 
 
 @dataclass
 class SystolicUnit:
-    in_a: int = 0
-    in_b: int = 0
-    out_a: int = 0
-    out_b: int = 0
+    a: int = 0
+    b: int = 0
+    out_a: Optional["SystolicUnit"] = None
+    out_b: Optional["SystolicUnit"] = None
     c: int = 0
 
 
@@ -18,26 +20,30 @@ class SystolicArray:
     def __init__(self):
         self.array = [[SystolicUnit() for _ in range(4)] for _ in range(4)]
 
-    def update_inputs(self, aa: list[int], bb: list[int]):
-        """
-        Pipe numbers from one cell's output to the next cell's input,
-        or from the input arrays to the topmost/leftmost cells' inputs
-        """
+        # "Wiring" the units together in a grid
         for r, c in product(range(4), range(4)):
-            if c == 0:
-                self.array[r][c].in_a = aa[r]
-            else:
-                self.array[r][c].in_a = self.array[r][c - 1].out_a
-            if r == 0:
-                self.array[r][c].in_b = bb[c]
-            else:
-                self.array[r][c].in_b = self.array[r - 1][c].out_b
+            if c + 1 < 4:
+                self.array[r][c].out_a = self.array[r][c + 1]
+            if r + 1 < 4:
+                self.array[r][c].out_b = self.array[r + 1][c]
+
+    def update_inputs(self, aa: list[int], bb: list[int]):
+        """Update inputs of leftmost/uppermost cells"""
+        for r in range(4):
+            self.array[r][0].a = aa[r]
+        for c in range(4):
+            self.array[0][c].b = bb[c]
 
     def run_cells(self):
-        for r, c in product(range(4), range(4)):
-            self.array[r][c].c += self.array[r][c].in_a * self.array[r][c].in_b
-            self.array[r][c].out_a = self.array[r][c].in_a
-            self.array[r][c].out_b = self.array[r][c].in_b
+        # Running in this exact order (large to small r and c) is important
+        # so values aren't moved multiple times in one cycle
+        for r, c in product(reversed(range(4)), reversed(range(4))):
+            cell = self.array[r][c]
+            cell.c += cell.a * cell.b
+            if cell.out_a:
+                cell.out_a.a = cell.a
+            if cell.out_b:
+                cell.out_b.b = cell.b
 
     def matmul(self, A: np.ndarray, B: np.ndarray) -> np.ndarray:
         assert A.shape == B.shape == (4, 4)
